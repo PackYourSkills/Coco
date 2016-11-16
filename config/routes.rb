58b5a2e22e3e993ab5ec58809665120483e87b75
Maxime BDB <maxime@packyourskills.com>
2016-11-16 11:14:28 +0100

    user fonctionnal

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :users, only: [:update, :show, :edit]
+
+  resources :nests, only: [:new, :create, :show, :edit, :destroy]
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
