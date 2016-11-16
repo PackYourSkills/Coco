3ba82080302f25408b588990c5c7db2181c44de3
celia albano <albano.celia@gmail.com>
2016-11-16 11:54:33 +0100

    booking model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116101215) do
+ActiveRecord::Schema.define(version: 20161116104437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date     "date"
+    t.integer  "number"
+    t.integer  "user_id"
+    t.integer  "nest_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["nest_id"], name: "index_bookings_on_nest_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
 
   create_table "nests", force: :cascade do |t|
     t.integer  "max_capacity"
@@ -73,5 +84,7 @@ ActiveRecord::Schema.define(version: 20161116101215) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bookings", "nests"
+  add_foreign_key "bookings", "users"
   add_foreign_key "nests", "users"
 end
